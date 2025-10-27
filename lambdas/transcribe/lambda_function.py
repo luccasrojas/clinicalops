216d55ec4643d09367ddd7990af450bf9921a0c7
luccasrojas <luccasrojas@gmail.com>
2025-10-26 19:07:30 -0500

    transcribe

--- lambdas/transcribe/lambda_function.py
+++ lambdas/transcribe/lambda_function.py
@@ -1,3 +1,30 @@
+import os
+from dotenv import load_dotenv
+import assemblyai as aai
+
+load_dotenv()
+
+ASSEMBLY_KEY = os.getenv("ASSEMBLY_KEY")
+
+def transcribe_audio(audio_url):
+    aai.settings.api_key = ASSEMBLY_KEY
+
+    config = aai.TranscriptionConfig(speech_model=aai.SpeechModel.universal, speaker_labels=True, language_code="es", speakers_expected=2)
+
+    transcript = aai.Transcriber(config=config).transcribe(audio_url)
+    if transcript.status == "error":
+        raise RuntimeError(f"Transcription failed: {transcript.error}")
+
+    full_text = ""
+    for utterance in transcript.utterances:
+        speaker = f"Speaker{utterance.speaker}"
+        text = utterance.text
+        
+        full_text += f"{speaker}: {text}\n\n"
+    
+    return full_text
+
 def lambda_handler(event, context):
-    print(event)
-    return {"statusCode": 200, "body": "OK"}
+    audio_url = event['audio_url']
+    transcribed_text = transcribe_audio(audio_url)
+    return {"statusCode": 200, "body": transcribed_text}
